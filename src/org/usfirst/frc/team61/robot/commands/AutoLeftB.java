019848f8aa76cf0926fcf050fbe38f037a61190c
Thomas Buckley <tbuckley@tabwebsites.com>
2018-04-10 15:13:29 -0400

    lowered forward movement for left and right side

--- src/org/usfirst/frc/team61/robot/commands/AutoLeftB.java
+++ src/org/usfirst/frc/team61/robot/commands/AutoLeftB.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoLeftB extends CommandGroup {
 
     public AutoLeftB() {
-    	addSequential(new DriveForDistance(290, .8, 10));
+    	addSequential(new DriveForDistance(195, .8, 10));
     	addSequential(new DriveForDistance(10, .3, 5));
     }
 }
